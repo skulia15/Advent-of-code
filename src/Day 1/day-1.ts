fa7b364f1ec0fb486e37634ae7b17f8b2908afe8
Skúli Arnarsson <skuli.arnarson@advania.is>
2022-12-01 09:28:55 +0000

    day 1

---
+++ src/Day 1/day-1.ts
@@ -0,0 +1,13 @@
+import { readFileSync } from 'fs'
+
+const result = readFileSync('./puzzle-input.txt', 'utf-8')
+
+const resultSplit = result.split('\r\n\r\n')
+const listList = resultSplit.map(x => {
+  return x.split('\r\n')
+})
+
+const sumArray = listList.map(x => {
+  return x.reduce((partialSum, a) => partialSum + parseInt(a), 0)
+})
+console.log(Math.max(...sumArray))
