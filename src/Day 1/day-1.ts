6609068bef2fadec5f6693db02e2353fcf6f9495
Skúli Arnarsson <skulia15@ru.is>
2022-12-03 21:32:57 +0000

    day 3

--- src/Day 1/day-1.ts
+++ src/Day 1/day-1.ts
@@ -1,13 +1,13 @@
-import { readFileSync } from 'fs'
+import { readFileSync } from 'fs';
 
-const result = readFileSync('./puzzle-input.txt', 'utf-8')
+const result = readFileSync('./puzzle-input.txt', 'utf-8');
 
-const resultSplit = result.split('\r\n\r\n')
-const listList = resultSplit.map(x => {
-  return x.split('\r\n')
-})
+const resultSplit = result.split('\r\n\r\n');
+const listList = resultSplit.map((x: string) => {
+    return x.split('\r\n');
+});
 
-const sumArray = listList.map(x => {
-  return x.reduce((partialSum, a) => partialSum + parseInt(a), 0)
-})
-console.log(Math.max(...sumArray))
+const sumArray = listList.map((x) => {
+    return x.reduce((partialSum, a) => partialSum + parseInt(a), 0);
+});
+console.log(Math.max(...sumArray));
